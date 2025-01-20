5b95b778ff99de06dc6dad1a7bfaef0bd95d1fdd
Sakshi <sakshi.birman.s@gmail.com>
2025-01-20 12:31:34 +0530

    Send the Data to HTML

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class MyTask(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     content =db.Column(db.String(100), nullable=False)
     complete =db.Column(db.Integer, default=0)
-    created=db.Column(db.DateTime, default=lambda: datetime.datetime.now(ist))
+    created=db.Column(db.DateTime, default=lambda: datetime.now(ist))
 
     def __repr__(self) -> str:
         return f"Task {self.id}"
@@ -27,7 +27,7 @@ class MyTask(db.Model):
 @app.route("/", methods=["POST","GET"])
 def index():
     #Add a task
-    if request=="POST":
+    if request.method=="POST":
         current_task=request.form['content']
         new_task=MyTask(content=current_task)
         try:
@@ -42,15 +42,6 @@ def index():
         tasks = MyTask.query.order_by(MyTask.created).all()
 
         return render_template("index.html", tasks=tasks)
-
-
-
-
-
-
-
-
-
 
 if __name__ in "__main__":
     with app.app_context():
